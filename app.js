06a01a6b3152f7eb068c4801e2b09054cfd66eb7
froliceric0914 <wzhao.eric@gmail.com>
2021-02-03 16:22:30 -0500

    feat: add form submit handler

--- app.js
+++ app.js
@@ -8,7 +8,16 @@
     // Create Human Object
 
     // Use IIFE to get human data from form
-
+    const getHumanData = (function (){
+        const humanData = {
+            name: document.getElementById("name"),
+            height: document.getElementById("feet") + document.getElementById("inches"),
+            weight: document.getElementById("inches"),
+            diet: document.getElementById("diet"),
+        }
+        
+        return humanData
+    })()
 
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
@@ -30,3 +39,12 @@
 
 
 // On button click, prepare and display infographic
+function logSubmit(event) {
+    event.preventDefault();
+    console.log("hello")
+  }
+  
+
+  (function (){
+      document.getElementById('btn').addEventListener('click', logSubmit)
+  })()
